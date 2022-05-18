054a2c20e545576f99ddd3b21154d7c4f6a4143f
Anna G <gumulka@student.agh.edu.pl>
2022-05-19 00:37:24 +0200

    poporawa wyglądu

--- plik.js
+++ plik.js
@@ -1,107 +1,117 @@
 var start = false; //klikanie przycisku
-	var click = false; // klikanie po polu
-	var timeColour;
-	var timeClick;
-	var avgTime;
-	var minTime = 999999;
-	var maxTime = 0;
-	var sumTime =0;
-	var colours = ['green', 'blue', 'yellow', 'red', 'magenta', 'cyan', 'grey', 'pink'];
-	var prevColour = 100;
-	
-	var gameStep;
-	var num = 5;
-	
-	// ------------- FUNKCJA WYCZYSZCZENIA GRY --------------------
-function clearGame(){
-start = false;
-gamestep = 0;
-sumTime =0;
-avgTime = undefined;
-document.getElementById('przycisk').innerHTML="Start";
-document.getElementById('pole-gry').style['background-color'] = document.fgColor;
-document.getElementById('pole-gry').style['display'] = 'none';
-document.getElementById('pole-gry').innerHTML="";
+var click = false; // klikanie po polu
+var timeColour;
+var timeClick;
+var avgTime;
+var minTime = 999999;
+var maxTime = 0;
+var sumTime = 0;
+var colours = ['green', 'blue', 'yellow', 'red', 'magenta', 'cyan', 'grey', 'pink'];
+var prevColour = 100;
 
-document.getElementById('ustawienia').style['display'] = 'block';
-document.getElementById('statystyki').style['display'] = 'block';
+var gameStep;
+var num = 5;
+
+// ------------- FUNKCJA WYCZYSZCZENIA GRY --------------------
+function clearGame() {
+	start = false;
+	gamestep = 0;
+	sumTime = 0;
+	avgTime = undefined;
+	document.getElementById('przycisk').innerHTML = "Start";
+	document.getElementById('pole-gry').style['background-color'] = document.fgColor;
+	document.getElementById('pole-gry').style['display'] = 'none';
+	document.getElementById('pole-gry').innerHTML = "";
+
+	document.getElementById('ustawienia').style['display'] = 'block';
+	document.getElementById('statystyki').style['display'] = 'none';
+	//document.getElementById('sredni').innerHTML = "&nbsp;";
+	//document.getElementById('najdluzszy').innerHTML = "&nbsp;";
+	//document.getElementById('najkrotszy').innerHTML = "&nbsp;";
 
 }
 // ------------- FUNKCJA USTAWIANIA PARAMETROW --------------------
-function load(){
-document.getElementById('proby').value=num;
+function load() {
+	document.getElementById('proby').value = num;
 }
 
 // ------------- FUNKCJA ZMIANY PARAMETROW --------------------	
-function changeNum(){
-num = parseInt(document.getElementById('proby').value);
+function changeNum() {
+	num = parseInt(document.getElementById('proby').value);
 }
 
 // ------------- FUNKCJA ZMIANY STATYSTYK --------------------	
-function displayStat(){
-document.getElementById('sredni').innerHTML=avgTime;
-document.getElementById('najdluzszy').innerHTML=maxTime;
-document.getElementById('najkrotszy').innerHTML=minTime;
+function displayStat() {
+	document.getElementById('sredni').innerHTML = avgTime;
+	document.getElementById('najdluzszy').innerHTML = maxTime;
+	document.getElementById('najkrotszy').innerHTML = minTime;
 }
 // ------------- FUNKCJA ZMIANY KOLORU POLA GRY --------------------	
-function changeColour(){
-var randomColour;
- do {
- randomColour = parseInt(Math.random()* 10);}
- while (randomColour<= 0 || randomColour>colours.length || randomColour==prevColour);
- prevColour=randomColour;
- document.getElementById('pole-gry').style['background-color'] = colours[randomColour];
+function changeColour() {
+	var randomColour;
+	do {
+		randomColour = parseInt(Math.random() * 10);
+	}
+	while (randomColour <= 0 || randomColour > colours.length || randomColour == prevColour);
+	prevColour = randomColour;
+	console.log('changeColour, new color:' + randomColour);
+	document.getElementById('pole-gry').style['background-color'] = colours[randomColour];
 
 }
 // ------------- FUNKCJA OBLICZANIA ŚREDNIEJ --------------------
-function funAvgTime(){
-var timeMs = timeClick - timeColour;
-sumTime +=timeMs;
-avgTime = sumTime/(gameStep+1); 
-if (timeMs > maxTime) maxTime=timeMs;
-if (timeMs < minTime) minTime=timeMs;
+function funAvgTime() {
+	var timeMs = timeClick - timeColour;
+	console.log("clicked after " + timeMs + " [ms]");
+	sumTime += timeMs;
+	avgTime = sumTime / (gameStep + 1);
+	if (timeMs > maxTime) maxTime = timeMs;
+	if (timeMs < minTime) minTime = timeMs;
 }
 
 // ------------- FUNKCJA ODCZYTANIA CZASU KLIKNIECIA --------------------
-function readTime(){
-if (!click) {click = true;
-timeClick = Date.now();
-funAvgTime();
-displayStat();
-document.getElementById('statystyki').style['display'] = 'block';
+function readTime() {
+	if (!click) {
+		click = true;
+		console.log("clicked");
+		timeClick = Date.now();
+		funAvgTime();
+		displayStat();
+		document.getElementById('statystyki').style['display'] = 'block';
 
 
-}
+	}
 }
 
 // ------------- FUNKCJA ZMIANY CZASU PO KTÓRYM NASTĘPUJE ZMIANA KOLORU POLA GRY --------------------
-function timeToChangeColour(){
-console.log('timeToChangeColour');
-    changeColour();
+function timeToChangeColour() {
+	console.log('===============   timeToChangeColour');
+	changeColour();
 	timeColour = Date.now();
 	click = false;
-    gameStep++;
-    if(gameStep < num){
-        var randomTime = parseInt(Math.random()*3000+1000);
+	gameStep++;
+	console.log('gameStep:' + gameStep);
+	if (gameStep < num) {
+		var randomTime = parseInt(Math.random() * 3000 + 1000);
 		//document.getElementById('kontrola').innerHTML=randomTime;
 		setTimeout(() => timeToChangeColour(), randomTime);
-        //setTimeout(timeToChangeColour, randomTime);
-    }
-	else {document.getElementById('pole-gry').innerHTML="Gra zakończona";
-	document.getElementById('pole-gry').style['background-color'] = document.fgColor;
-	setTimeout(() => clearGame(), 2000);
+		//setTimeout(timeToChangeColour, randomTime);
 	}
-	
-	
-}
-	
-// ------------- FUNKCJA URUCHOMIENIA GRY --------------------
-function playGame(){
-gameStep = 0;
-document.getElementById('pole-gry').innerHTML="";
+	else {
+		document.getElementById('pole-gry').innerHTML = "Gra zakończona";
+		document.getElementById('pole-gry').style['background-color'] = document.fgColor;
+		setTimeout(() => clearGame(), 2000);
+	}
 
-    timeToChangeColour();  
-	
+
+}
+
+// ------------- FUNKCJA URUCHOMIENIA GRY --------------------
+function playGame() {
+	gameStep = 0;
+	document.getElementById('pole-gry').innerHTML = "";
+
+	timeToChangeColour();
+
 
 
 }
@@ -110,16 +120,19 @@ document.getElementById('pole-gry').innerHTML="";
 
 
 // ------------- FUNKCJA OBSŁUGI PRZYCISKU --------------------
-	function startStopGame(){
-if (start == false){
-start = true;
-click = false;
+function startStopGame() {
+	if (start == false) {
+		start = true;
+		click = false;
 
-document.getElementById('przycisk').innerHTML="STOP";
-document.getElementById('pole-gry').style['display'] = 'block';
-document.getElementById('pole-gry').innerHTML="Gra rozpoczęta";
-document.getElementById('ustawienia').style['display'] = 'none';
-setTimeout(() => playGame(), 2000);
-}
-else clearGame();
+
+		document.getElementById('przycisk').innerHTML = "STOP";
+		document.getElementById('pole-gry').style['display'] = 'block';
+		document.getElementById('pole-gry').innerHTML = "Gra rozpoczęta";
+		//document.getElementById('ustawienia').style['display'] = 'none';
+		setTimeout(() => playGame(), 2000);
+	}
+	else {clearGame();
+
+	}
 }
